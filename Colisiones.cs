0f6b07700c4c1a371b183b267bfe5176fc4efcf0
artfofo <51091916+artfofo@users.noreply.github.com>
2020-06-27 17:19:52 -0500

    Update Colisiones.cs

--- Colisiones.cs
+++ Colisiones.cs
@@ -6,7 +6,7 @@ public class Colisiones : MonoBehaviour
 {
    // string nombreOBJ;
 
-
+//aqui tuve errro porque no registra la parte de registrocolision que tengo en el primer scrip que realizamos
    // private void OnCollisionEnter2D(Collision2D collision)
    // {
      //   var EncontrarBob = GetComponentInParent<Player1>();
